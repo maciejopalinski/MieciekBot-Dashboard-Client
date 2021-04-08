e9504ceced82f6aa6e215f43002e69f8b0c54d2b
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-08 15:08:18 +0200

    Add UserData class

---
+++ src/data/UserData.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+import { BaseDataHandler, API, CDN } from './BaseHandler';
+import { IUser } from './Types';
+
+export class UserData extends BaseDataHandler<IUser> {
+    
+    async handler() {
+        const general = await axios.get(
+            API('/discord/@me'), { withCredentials: true }
+        );
+
+        const mutual_guilds = await axios.get(
+            API('/discord/guilds/mutual'), { withCredentials: true }
+        );
+        
+        const user: IUser = general.data;
+        user.mutual_guilds = mutual_guilds.data;
+        return user;
+    }
+
+    constructor() {
+        super('API_User', { retry: false });
+        this.fetch(this.handler);
+    }
+
+    getAvatar(size = 512): string | undefined {
+        if(this.isLoading) return undefined;
+        
+        let { id, avatar, discriminator } = this.data!;
+        
+        if(!avatar) return CDN(`/embed/avatars/${+discriminator % 5}.png?size=${size}`);
+        else return CDN(`/avatars/${id}/${avatar}.jpg?size=${size}`);
+    }
+}
