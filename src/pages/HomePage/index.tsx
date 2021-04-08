3d39127949abfd2b2c1c118e9959328398590606
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-08 15:10:14 +0200

    Add examples (commented) into HomePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+// import { Spinner } from 'react-bootstrap';
+
+import { UserData } from '../../data';
 
 import './styles.css';
 
-export const HomePage = () => {
-    
+export const HomePage = ({ user } : { user: UserData }) => {
+
     return (
         <div className='app'>
             <img
@@ -14,6 +17,21 @@ export const HomePage = () => {
             />
             <h1 className='mieciekbot-name'>MieciekBot</h1>
             <p className='mieciekbot-desc'>Includes moderation, leveling, music and much more!</p>
+
+            {/* TODO: delete this debug line, it will be moved to dashboard page */}
+            {/* <div>
+                { user.error?.response?.status == 429 && `Too many requests. Stop it! ` }
+                { user.isError && `You are not logged in.` }
+                { user.isLoading && <Spinner animation='border' /> }
+                { user.isSuccess && `${user.data?.username}#${user.data?.discriminator}` }
+                { user.isSuccess &&
+                    <div className='mutual-guilds'>
+                        {user.data?.mutual_guilds?.map(g => <p key={g.id}>{g.name}</p>)}
+                    </div>
+                }
+            </div> */}
+
+            { user.isSuccess && user.getAvatar(1024) }
         </div>
     );
 }
