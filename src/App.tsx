1d2da944c7ad9373e56c9b0a3aad0e5d4a6990f3
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-08 15:09:35 +0200

    Add user data fetching and 404 redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import { HomePage } from './pages';
 import { Navbar } from './components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { UserData } from './data';
+
 
 function App() {
-    
+
+    const user = new UserData();
+
     return (
         <Router>
-            
             <Navbar />
 
             <Switch>
                 
                 <Route exact path='/'>
-                    <HomePage />
+                    <HomePage user={user} />
+                </Route>
+
+                <Route path='*'>
+                    <Redirect to='/' />
                 </Route>
             
             </Switch>
