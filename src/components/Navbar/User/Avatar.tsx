f7d766678ea96750e409caf2c2ad7c8a4f2a9de3
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-09 08:40:21 +0200

    Fix avatar on small screens

--- src/components/Navbar/User/Avatar.tsx
+++ src/components/Navbar/User/Avatar.tsx
@@ -5,6 +5,6 @@ import './Avatar.css';
 export const UserAvatar = ({ user } : { user: UserData }) => {
     
     return (
-        <img className='user-avatar' src={user.getAvatar()} alt='User Avatar'></img>
+        <img className='user-avatar d-none d-lg-block' src={user.getAvatar()} alt='User Avatar'></img>
     );
 }
