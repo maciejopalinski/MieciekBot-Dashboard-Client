2f7588d42631c3fb2290d6114142716b956a3575
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-09 08:43:10 +0200

    Add GuildIcon component

---
+++ src/components/Dashboard/Guild/GuildIcon.tsx
@@ -0,0 +1,23 @@
+import { GuildData } from "../../../data/GuildData";
+
+import './GuildIcon.css';
+
+export const GuildIcon = ({ guild } : { guild: GuildData }) => {
+    
+    if(guild.hasIcon()) {
+        return (
+            <img
+                className='guild-icon guild-image'
+                src={guild.getIcon(512)}
+                alt={`${guild.data?.name} Icon`}
+            />
+        );
+    }
+    else {
+        return (
+            <div className='guild-icon guild-acronym'>
+                {guild.data?.name.split(' ').map(v => v[0]).join('')}
+            </div>
+        );
+    }
+}
