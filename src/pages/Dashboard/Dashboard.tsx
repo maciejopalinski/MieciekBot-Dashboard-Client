6f4791eada915debb588a9cf6a30d900afff2ba3
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-09 08:44:28 +0200

    Add Dashboard page (example)

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import { useParams } from 'react-router';
+import { UserData } from '../../data';
+
+import './Dashboard.css';
+
+export const DashboardPage = ({ user } : { user: UserData }) => {
+
+    let { id } = useParams<{ id: string }>();
+
+    return (
+        <div className='app'>
+            <h1>Dashboard (ID: {id})</h1>
+        </div>
+    );
+}
