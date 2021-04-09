10a20dfa3edcad5a092e9b3a2974038249d83376
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-09 13:16:55 +0200

    Add dummy Dashboard components
    (as a structure for future dev)

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,8 @@
+import { GuildData, UserData } from "../../data";
+
+export const Dashboard = ({ user, guild } : { user: UserData, guild: GuildData }) => {
+
+    return (
+        <h1>Dashboard</h1>
+    );
+}
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,3 @@
+export * from './Dashboard';
+export * from './Popup';
+export * from './Form';
