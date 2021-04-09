5741d3166bb9c64b73a2d19f687b6d116166aa6c
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-09 08:43:22 +0200

    Add Guild component

---
+++ src/components/Dashboard/Guild/Guild.tsx
@@ -0,0 +1,26 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { GuildIcon } from './';
+import { GuildData } from "../../../data";
+
+import './Guild.css';
+
+export const Guild = ({ guild } : { guild: GuildData }) => {
+
+    const { id, name } = guild.data!;
+    const link = `/dashboard/${id}`;
+    const btn_id = `btn-dashboard-guild-${id}`;
+
+    return (
+        <div className='guild' onClick={() => document.getElementById(btn_id)?.click()}>
+
+            <div className='guild-info'>
+                <GuildIcon guild={guild} />
+                <h3 className='guild-name'>{name}</h3>
+            </div>
+
+            <Button variant='success' size='sm' as={Link} to={link} id={btn_id}>Go to Dashboard</Button>
+
+        </div>
+    );
+}
