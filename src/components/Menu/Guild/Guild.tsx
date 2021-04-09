8e4604f1c841956223de70bf29cb223775fbbced
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-09 09:11:24 +0200

    Rename components (Dashboard > Menu)

--- src/components/Menu/Guild/Guild.tsx
+++ src/components/Menu/Guild/Guild.tsx
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { GuildIcon } from './';
+import { GuildIcon } from '.';
 import { GuildData } from "../../../data";
 
 import './Guild.css';
