af5c39d2b00c5351c9276a5a4b84761e8dde54c7
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-12 07:48:53 +0200

    Add useWindowSize hook

---
+++ src/hooks/useWindowSize.tsx
@@ -0,0 +1,17 @@
+import { useLayoutEffect, useState } from 'react';
+
+export const useWindowSize = () => {
+    
+    const [size, setSize] = useState([0, 0]);
+    const updateSize = () => setSize([window.innerWidth, window.innerHeight]);
+
+    useLayoutEffect(() => {
+
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+
+    }, []);
+
+    return size;
+}
