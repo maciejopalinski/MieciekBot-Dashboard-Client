8fb2f8f30256ab53454e7d37d613d1daa58c9916
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-13 08:00:21 +0200

    Change styles in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,20 @@
 import { ConfirmPopup } from '../../components/Dashboard';
 import { UserData } from '../../data';
 
-import './Home.css';
-
 export const HomePage = ({ user } : { user: UserData }) => {
 
     return (
-        <main className='app'>
+        <main className='app center'>
 
             <img
-                className='mieciekbot-img'
+                className='mieciekbot-avatar'
                 src='https://cdn.discordapp.com/avatars/511219391539445761/067aec30675c8483654c344bfabd2c19.png?size=4096'
                 alt='MieciekBot Avatar'
                 draggable={false}
             />
-            <h1 className='mieciekbot-name'>MieciekBot</h1>
-            <p className='mieciekbot-desc'>Includes moderation, leveling, music and much more!</p>
-
-            <h1>H1</h1>
-            <h2>H2</h2>
-            <h3>H3</h3>
-
+            <h1 className='header-home'>MieciekBot</h1>
+            <p className='description-home'>Includes moderation, leveling, music and much more!</p>
+            
             <ConfirmPopup resetAction={() => console.log('reset')} />
 
         </main>
