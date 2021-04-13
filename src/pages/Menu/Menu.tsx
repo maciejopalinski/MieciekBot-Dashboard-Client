fccba7fa74343b7908f1635753adc916ab2c73eb
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-13 08:00:06 +0200

    Fix Menu page to use new data handlers

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,14 +1,16 @@
-import { UserData } from '../../data';
 import { GuildsWrapper } from '../../components';
+import { UserData, MutualGuildsData } from '../../data';
 
 export const MenuPage = ({ user } : { user: UserData }) => {
 
+    const mutual_guilds = new MutualGuildsData();
+
     return (
-        <main className='app'>
+        <main className='app center'>
 
             <h1>Menu</h1>
 
-            <GuildsWrapper user={user} />
+            <GuildsWrapper guilds={mutual_guilds} />
 
         </main>
     );
