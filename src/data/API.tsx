c39a1937d29e6bb4aa7cd05877c9178c741c4115
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-05 22:46:31 +0200

    Add submitGuildConfig() function

--- src/data/API.tsx
+++ src/data/API.tsx
@@ -6,34 +6,33 @@ export const CDN = (endpoint: string) => 'https://cdn.discordapp.com' + endpoint
 
 export const fetchUser = async () => {
 
-    const data = await axios.get<IUser>(
+    return await axios.get<IUser>(
         API('/discord/@me'),
         { withCredentials: true}
     );
-    return data;
 }
 
 export const fetchMutualGuilds = async () => {
 
-    const data = await axios.get<IGuild[]>(
+    return await axios.get<IGuild[]>(
         API('/discord/guilds/mutual'),
         { withCredentials: true }
     );
-    return data;
 }
 
 export const fetchGuildConfig = async (id: string) => {
 
-    const data = await axios.get<IGuildConfig>(
+    return await axios.get<IGuildConfig>(
         API(`/discord/guilds/${id}/config`),
         { withCredentials: true }
     );
-    return data;
 }
 
 export const submitGuildConfig = async (id: string, data: IGuildConfig) => {
 
-    // TODO: add POST request to send updated guild config
-    
-    console.log(data);
+    return await axios.post(
+        API(`/discord/guilds/${id}/config`),
+        data,
+        { withCredentials: true }
+    );
 }
