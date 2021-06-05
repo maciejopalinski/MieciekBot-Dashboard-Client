0ad2c0bb9169af81afefc9f660984048ee4dd068
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-05 22:46:49 +0200

    Add guild name to guild dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router';
 import { useQuery } from 'react-query';
-import { fetchGuildConfig } from '../../data';
+import { fetchGuildConfig, fetchMutualGuilds } from '../../data';
 import { DashboardForm, Spinner } from '../../components';
 
 export const DashboardPage = () => {
@@ -8,13 +8,17 @@ export const DashboardPage = () => {
     const { id } = useParams<{ id: string }>();
 
     const { data, isSuccess } = useQuery(`/guilds/${id}/config`, () => fetchGuildConfig(id));
+    const mutual_guilds = useQuery(`/guilds/mutual`, fetchMutualGuilds);
 
-    if(isSuccess) {
+    if(isSuccess && mutual_guilds.isSuccess) {
 
         const guild_config = data!.data;
+        const guild_discord_data = mutual_guilds.data.data.find(g => g.id === id)!;
 
         return (
             <main className='app'>
+
+                <h1>{guild_discord_data.name} - Dashboard</h1>
 
                 <DashboardForm config={guild_config} />
 
