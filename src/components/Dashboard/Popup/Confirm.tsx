98960554be8a358044f216e17e99812e1b72cba0
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-05 22:46:14 +0200

    Update styles for ConfirmPopup

--- src/components/Dashboard/Popup/Confirm.tsx
+++ src/components/Dashboard/Popup/Confirm.tsx
@@ -1,17 +1,15 @@
 import { Button } from 'react-bootstrap';
 import './Confirm.css';
 
-type ConfirmPopupProps = {
-    message?: string;
-    isBtnDisabled?: boolean;
-    resetAction?: any;
-}
-
 export const ConfirmPopup = ({
     message = 'Careful - you have unsaved changes!',
     isBtnDisabled = false,
-    resetAction = null
-} : ConfirmPopupProps) => {
+    resetAction
+} : {
+    message?: string,
+    isBtnDisabled?: boolean,
+    resetAction?: any
+}) => {
 
     return (
         <div className='confirm-wrapper'>
